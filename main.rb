6567cf608322acbe2ba97406d7b8ebf838a1f179
Edem <keltikan@gmail.com>
2019-02-14 23:33:43 +0300

    Type checking was added

---
+++ main.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/credit_card'
+require_relative 'lib/credit_card_checker'
+
+card_number = ARGV[0]
+credit_card = CreditCard.new(card_number)
+credit_card_checker = CreditCardChecker.new(credit_card)
+p credit_card_checker.check_type
