8f7a27cbb5baae3b26e7f30128f92d72243e6449
Edem <keltikan@gmail.com>
2019-02-15 11:45:14 +0300

    RSpec improved

--- spec/credit_card_checker_spec.rb
+++ spec/credit_card_checker_spec.rb
@@ -1,30 +1,39 @@
 require 'credit_card_checker'
 
 RSpec.describe CreditCardChecker do
-  let(:cards) { %w(4417123456789112
-                   371234567890123
-                   6011123456789012
-                   5412345678901234
-                   639002629001853224) }
-
   describe '.identify_type' do
-    let(:types) { cards.map { |card_number| CreditCardChecker.identify_type(card_number) } }
+    context 'input is correct' do
+      let(:cards) { %w(371234567890123
+                       6011123456789012
+                       5412345678901234
+                       4417123456789112
+                       1234567890123456) }
+      it 'should correctly identify card type' do
+        types = cards.map { |card_number| CreditCardChecker.identify_type(card_number) }
+        expect(types).to eq %w(AMEX Discover MasterCard Visa unknown)
+      end
+    end
 
-    it 'should correctly identify card type' do
-      expect(types).to eq %w(Visa AMEX Discover MasterCard unknown)
+    context 'input is incorrect' do
+      let(:incorrect) { 'jfkdmvpie;w' }
+      it 'should return nil' do
+        expect(CreditCardChecker.identify_type(incorrect)).to be_nil
+      end
     end
   end
 
   describe '.check_validity' do
     context 'card is valid' do
-      it 'it should issue that the card is valid' do
-        expect(CreditCardChecker.check_validity(cards.last)).to eq 'valid'
+      let(:valid_card) { '4408041234567893' }
+      it 'should issue that the card is valid' do
+        expect(CreditCardChecker.check_validity(valid_card)).to eq 'valid'
       end
     end
 
     context 'card is not valid' do
-      it 'it should issue that the card is not valid' do
-        expect(CreditCardChecker.check_validity(cards.first)).to eq 'not valid'
+      let(:invalid_card) { '4417123456789112' }
+      it 'should issue that the card is not valid' do
+        expect(CreditCardChecker.check_validity(invalid_card)).to eq 'not valid'
       end
     end
   end
